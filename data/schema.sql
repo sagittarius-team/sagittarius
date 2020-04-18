ce7e0f849d742d037a1127fd264f722c8d87e7be
abdallahshanaah <abdallah_m_sh@yahoo.com>
2020-04-18 22:12:07 +0300

    add future render

--- data/schema.sql
+++ data/schema.sql
@@ -1,5 +1,5 @@
 DROP TABLE IF EXISTS mission;
-
+DROP TABLE if EXISTS outlook;
 CREATE TABLE mission (
     id SERIAL PRIMARY KEY,
     year VARCHAR(255),
@@ -8,4 +8,12 @@ CREATE TABLE mission (
     name VARCHAR(255),
     vidURL VARCHAR(255),
     img VARCHAR(255)
+);
+
+CREATE TABLE outlook (
+    name VARCHAR (255),
+    image VARCHAR(255),
+    net VARCHAR(255),
+    -- agencies VARCHAR(255),
+    description TEXT 
 );
