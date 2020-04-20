6656521ea9521ab5a8e40343ff9e3288fa28ccd8
sondoshassan <sondos199628@gmail.com>
2020-04-20 09:15:02 +0300

    finish backend

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const client = new pg.Client(process.env.DATABASE_URL);
 //app using
 app.use(express.static('./public'));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(methodOverride('_method'));
 
@@ -24,113 +24,102 @@ app.set('view engine', 'ejs');
 
 /////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-app.get('/signup' , (req,res) =>{
-    res.render('/');
-});
-
-app.post('/add',addDataBase);//this route for post my data i hada insert it in sign page into my data pase
-function  addDataBase(req,res){
-// console.log(req.body);
-let {username,email,password,day,month,year,gender,isagree,criedt}=req.body;
-let SQL3 = 'SELECT (username,email) FROM signup WHERE username=$1 AND email=$2;';
-let safeValues2 = [username,email];
-client.query(SQL3,safeValues2)
-.then(result=>{
-    
-    if (result.rows[0]){
-        res.render('valid');
-    }
-    else {
-        let SQL='INSERT INTO signup (username,password,email,day,month,year,gender,isAgree,criedt) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9);';
-        let safeValues=[username,password,email,day,month,year,gender,isagree,criedt];
-        client.query(SQL ,safeValues)
-        .then(()=>{
-            let SQL2= 'SELECT * FROM signup WHERE username=$1;';
-            let safeValues3 = [username];
-        client.query(SQL2,safeValues3)
-        .then(result =>{
-            console.log(result.rows);
-            res.render('profile',{taskResult:result.rows[0]});
-        });
-        });
-    }
-
-});}
-
-
-app.get('/about',aboutUsPage);
+app.get('/about', aboutUsPage);
 app.get('/mission', desideDate);
 app.get('/show', pastMission);
 app.get('/f_mission', fulureMission);
-app.get('/book/:trip_id',booking);
-app.delete('/delete/:trip_id',deleteTripDataBase);
-
-function aboutUsPage(req,res){
-    res.render('aboutus');
-}
-
-
-
-
-
-
- app.get('/', renderProfile);
-
-app.get('/myProfile/:task_id' ,myprofile);
+app.get('/book/:trip_id', booking);
+app.delete('/delete/:trip_id', deleteTripDataBase);
+app.get('/all_book', renderBookDataBase);
+app.post('/add', addDataBase);
+app.get('/', homePage);
+app.get('/myProfile', myprofile);
+app.get('/login', loginpage);
+app.post('/', validLogIn);
+app.get('/signup',signUpPage);
+app.get('/logout',logutPage)
 
 
-function myprofile(req,res){
-    let SQL= 'SELECT * FROM signup ;';
+// Esraa 
+// render profile page
+function myprofile(req, res) {
+    let SQL = 'SELECT * FROM signin;';
     return client.query(SQL)
-    .then(result=>{
-
-       res.render('profile',{taskResult:result.rows[0]})
-    })
-
+        .then(result => {
+            if(result.rows[0]){
+                res.render('profile', { taskResult: result.rows[0] })
+            }
+            else {
+                res.render('login');
+            }
+        });
+}
+function signUpPage(req,res){
+    res.render('./layout/signup');
 }
 
- function  renderProfile(req,res){
-     let SQL= 'SELECT * FROM signup;';
-     return client.query(SQL)
-     .then(result=>{
-
-        res.render('index',{taskResult:result.rows})
-     })
-    }
-    
-
-app.get('/login',loginpage);
-
-
-
-function loginpage(req,res){
-
-res.render('login');
-
-
+function homePage(req, res) {
+  res.render('index');
 }
-app.post('/',validLogIn);
-function validLogIn(req,res){
+//this route for post my data i hada insert it in sign page into my data pase
+function addDataBase(req, res) {
+    // console.log(req.body);
+    let { username, email, password, day, month, year, gender, isagree, criedt } = req.body;
+    let SQL3 = 'SELECT (username,email) FROM signup WHERE username=$1 AND email=$2;';
+    let safeValues2 = [username, email];
+    client.query(SQL3, safeValues2)
+        .then(result => {
+
+            if (result.rows[0]) {
+                res.render('valid');
+            }
+            else {
+                let SQL = 'INSERT INTO signup (username,password,email,day,month,year,gender,isAgree,criedt) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9);';
+                let safeValues = [username, password, email, day, month, year, gender, isagree, criedt];
+                client.query(SQL, safeValues)
+                    .then(() => {
+                        let SQL2 = 'SELECT * FROM signup WHERE username=$1;';
+                        let safeValues3 = [username];
+                        client.query(SQL2, safeValues3)
+                            .then(() => {
+                                res.render('login');
+                            });
+                    });
+            }
+
+        });
+}
+function loginpage(req, res) {
+    res.render('login');
+}
+
+function validLogIn(req, res) {
     let username = req.body.username;
     let email = req.body.email;
     let SQL = 'SELECT * FROM signup WHERE username=$1 AND email=$2;';
-    let safeValues =[username ,email];
-    client.query(SQL,safeValues)
-    .then(result=>{
-if (result.rows[0]){
-   
-    res.render('profile',{taskResult:result.rows[0]});
-}else{
-    res.render('valid');
+    let safeValues = [username, email];
+    client.query(SQL, safeValues)
+        .then(result => {
+            if (result.rows[0]) {
+                    let object = result.rows[0];
+                    console.log('ddddddddddd   ',object);
+                    let SQL2 = 'INSERT INTO signIn (username,password,email,day,month,year,gender,isAgree,criedt) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9);';
+                    let safeValues2 = [object.username, object.password, object.email, object.day, object.month, object.year, object.gender, object.isagree, object.criedt];
+                    client.query(SQL2,safeValues2);
+                    myprofile(req, res);
+                }
+             else {
+                res.render('valid');
+            }
+        });
 }
-    })
+function logutPage(req,res){
+    let SQL = 'TRUNCATE TABLE signIn;';
+    client.query(SQL);
+    res.redirect('/');
 }
 
-
-
-
-
-
+//Yousef
 //////////////////to render mission page
 function desideDate(request, response) {
     response.render('./mission/mission');
@@ -149,15 +138,12 @@ function pastMission(request, response) {
             //if the date in data base render it from there
             if (results.rows.length > 0) {
                 console.log('in data base');
-                // console.log(results)
-                // console.log(results.rows)
                 response.render('./mission/show', { theJournies: results.rows })
             }
             else {
                 // if the date not in data base render it from API
                 console.log('from API')
                 let url = `https://launchlibrary.net/1.3/launch/${theDate}`;
-                // console.log(url);
                 superagent.get(url)
                     .then(data => {
                         let pastData = data.body.launches.map(val => {
@@ -165,80 +151,110 @@ function pastMission(request, response) {
                             let safeValues = [year, journy.date, journy.description, journy.name, journy.vidURL, journy.img];
                             let SQL2 = 'INSERT INTO mission (year,date,description,name,vidURL,img) VALUES ($1,$2,$3,$4,$5,$6);';
 
-                            client.query(SQL2,safeValues);
+                            client.query(SQL2, safeValues);
 
                             return journy
-                        })
+                        });
                         response.render('./mission/show', { theJournies: pastData });
 
-                    })
+                    });
             }
-        })
+        });
 }
 
+// Abdalluh
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 // fulure mission function 
-let allfultue;
 function fulureMission(req, res) {
     let sql = 'SELECT * FROM outlook';
-client.query(sql)  
-.then(data =>{
-    if(data.rows.length > 0 )
-    {
-        res.render('./fu_mission/f_mission',{all_future_moission:data.rows}); 
-    }
-    else 
-    { allfultue = [];
-        let url = `https://launchlibrary.net/1.3/launch/next/10`;
-    superagent.get(url)
+    client.query(sql)
         .then(data => {
-            console.log(data);
-         allfultue = data.body.launches.map(val => {
-                let future = new Fulure(val);
-                // let SQL = 'INSERT INTO outlook (name,net,image,description) VALUES ($1,$2,$3,$4);';
-                // let safeValues = [name,net,image,description];
-                // client.query(SQL,safeValues);
-                return future ;
-            });
-            res.render('./fu_mission/f_mission',{all_future_moission:allfultue}); 
+            if (data.rows.length > 0) {
+                console.log('DataBase Future');
+                res.render('./fu_mission/f_mission', { all_future_moission: data.rows });
+            }
+            else {
+                console.log('API Future');
+                let url = `https://launchlibrary.net/1.3/launch/next/10`;
+                superagent.get(url)
+                    .then(data => {
+                        data.body.launches.forEach(val => {
+                            let future = new Fulure(val);
+                            let cost = getRandomInt(1000000, 6000000);
+                            let SQL = 'INSERT INTO outlook (name,net,image,agencies,description,cost) VALUES ($1,$2,$3,$4,$5,$6);';
+                            let safeValues = [future.name, future.net, future.image, future.agencies, future.description, cost];
+                            client.query(SQL, safeValues);
+                            return future;
+                        });
+                    });
+                renderFutureMission(res);
+            }
         });
-    }
-});
 }
 
+function renderFutureMission(res) {
+    let SQL = 'SELECT * FROM outlook;';
+    client.query(SQL)
+        .then(result => {
+            res.render('./fu_mission/f_mission', { all_future_moission: result.rows });
+        });
+}
+
+// Sondos
+// about us page render
+function aboutUsPage(req, res) {
+    res.render('aboutus');
+}
 /// booking function
-function booking(req,res){
-    let idBook = req.params.trip_id;    
-
-    let name = allfultue[idBook].name;
-    
-    let net = allfultue[idBook].net;
-    let image = allfultue[idBook].image;
-    let description = allfultue[idBook].description;
-    let SQL = 'INSERT INTO booking (name,net,image,description) VALUES ($1,$2,$3,$4);';
-    let safeValues = [name,net,image,description];
-    client.query(SQL,safeValues);
-    renderBookDataBase(res);
-    
+function booking(req, res) {
+    let SQL2 = 'SELECT * FROM signIn;'
+    client.query(SQL2)
+    .then(result =>{
+        if(result.rows[0]){
+            let idBook = req.params.trip_id;
+            let SQL2 = 'SELECT * FROM outlook WHERE id=$1;';
+            let safeValues2 = [idBook];
+            client.query(SQL2, safeValues2)
+                .then(result => {
+                    let selection = result.rows[0];
+                    let SQL = 'INSERT INTO booking (name,net,image,agencies,description,cost) VALUES ($1,$2,$3,$4,$5,$6);';
+                    let safeValues = [selection.name, selection.net, selection.image, selection.agencies, selection.description, selection.cost];
+                    client.query(SQL, safeValues);
+                    renderBookDataBase(req,res);
+                });
+        }
+        else {
+            renderBookDataBase(req,res);
+            res.render('login');
+        }
+    })
+  
 }
-function renderBookDataBase(res) {
+
+function renderBookDataBase(req,res) {
     let SQL2 = 'SELECT * FROM booking;';
     client.query(SQL2)
         .then(result => {
+            console.log('to book ', result.rows);
             res.render('./fu_mission/show', { data: result.rows });
         });
 }
 
+
 // delete from dataBase
-function deleteTripDataBase(req,res){
-    console.log('hhhhhhhh hh ',req.params.trip_id);
+function deleteTripDataBase(req, res) {
     let idBook = req.params.trip_id;
     let SQL = 'DELETE FROM booking WHERE id = $1;';
     let safeValues = [idBook];
-    client.query(SQL,safeValues)
-    .then(() =>{
-       renderBookDataBase(res); 
-    }
-    );
+    client.query(SQL, safeValues)
+        .then(() => {
+            renderBookDataBase(req,res);
+        }
+        );
 }
 
 
@@ -254,14 +270,10 @@ function PastJourny(dataForOneJourny) {
 /////////////// constructor for  Fulure
 function Fulure(val) {
     this.name = val.name;
-    this.net=val.net;
-    this.image=val.rocket.imageURL;
-    // console.log(val.pads[0].agencies[0].name);
-    // this.agencies=val.pads[0].agencies[0].name;
-    
-
-    this.description=(val.missions[0] && val.missions[0].description)||'There is no description';
-
+    this.net = val.net;
+    this.image = val.rocket.imageURL;
+    this.agencies = val.location.pads[0].agencies[0].name || '';
+    this.description = (val.missions[0] && val.missions[0].description) || 'There is no description';
 }
 
 
