9cc166101a1d16dfe2e92650681ca8970720619e
esraaamal <ma.esraa@hotmail.com>
2020-04-20 03:17:33 +0300

    f

--- server.js
+++ server.js
@@ -20,15 +20,15 @@ app.use(express.urlencoded({extended:true}));
 
 app.use(methodOverride('_method'));
 
-//app sets
-app.set('view engine', 'ejs');
 
 app.use(bodyParser());
 app.set('view engine', 'ejs');
 
 app.use(express.static('./public'));
 app.set('views', [path.join(__dirname, 'views'),
-path.join(__dirname, 'views/layout/')
+path.join(__dirname, 'views/layout/'),
+path.join(__dirname, 'views/mission/'),
+path.join(__dirname, 'views/fu_mission/')
 ]);
 /////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
